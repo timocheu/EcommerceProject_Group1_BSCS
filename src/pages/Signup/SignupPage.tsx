dddbad7086fa423adbe89b2028734bdffa9f0bc2
timocheu <ccs.timotheoche@gmail.com>
2025-11-10 20:00:56 +0800

    Fix: signup page margin issue

--- src/pages/Signup/SignupPage.tsx
+++ src/pages/Signup/SignupPage.tsx
@@ -4,7 +4,7 @@ import { SignupForm } from "@/components/signup-form"
 
 export function SignupPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted flex flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
